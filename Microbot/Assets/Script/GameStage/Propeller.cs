5788ca25916c3efa1c1b47285881d2dd1f336a20
金原正和 <ebipilaf000@gmail.com>
2016-08-27 15:07:54 +0900

    プロペラ改良

--- Microbot/Assets/Script/GameStage/Propeller.cs
+++ Microbot/Assets/Script/GameStage/Propeller.cs
@@ -4,35 +4,57 @@ using System.Collections;
 public class Propellers : MonoBehaviour {
 
 	private GameObject _propeller;
-	public string ObjectName;
+	private GameObject _fan;
+	public string PropellerName;
+	public string FanName;
+
 	private GameObject _player;
+	private float _length;
 
 	bool _tracking_flag;
 
 	void Start ( ) {
-		_propeller = GameObject.Find( ObjectName ).gameObject;
+		_propeller = GameObject.Find( PropellerName ).gameObject;
+		_fan = GameObject.Find( FanName ).gameObject;
 		_player = GameObject.Find ("Player").gameObject;
 		_tracking_flag = false;
+		_length = 1.0f;
 	}
 
 	void Update ( ) {
 		judgeTracking( );
+		if (_tracking_flag) {
+			playerOnFlying();
+			setPosition();
+		}
 	}
 
 	private void judgeTracking( ) {
 		Vector3 player_pos = _player.transform.position;
+		player_pos.y = 0.0f;
 		Vector3 propeller_pos = _propeller.transform.position;
+		propeller_pos.y = 0.0f;
 
-		if ( player_pos == propeller_pos ) {
+		float distance = Vector3.Distance( player_pos, propeller_pos );
+
+		if ( distance < _length ) {
 			_tracking_flag = true;
 		}
 	}
 
 	private void setPosition( ) {
-		if ( _tracking_flag ) {
-			Vector3 player_pos = _player.transform.position;
-			player_pos.x += 0.5f;
-			_propeller.transform.position = player_pos;
-		}
+		Vector3 player_pos = _player.transform.position;
+		player_pos.y += 1.5f;
+		_propeller.transform.position = player_pos;
+	}
+
+	private void playerOnFlying( ) {
+		Vector3 player_pos = _player.transform.position;
+		player_pos.y += 1.0f * Time.deltaTime;
+		_player.transform.position = player_pos;
+	}
+
+	private bool isFanRange( ) {
+		return false;
 	}
 }
