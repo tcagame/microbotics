c8a081bcb4b12a61f1f49fe1d202061934d99383
sunshine <sunshine2333333@gmail.com>
2016-08-27 15:00:54 +0900

    テクスチャーのさしかえ

--- Microbot/Assets/Script/GamePlay/FanManager.cs
+++ Microbot/Assets/Script/GamePlay/FanManager.cs
@@ -3,18 +3,23 @@ using System.Collections;
 
 public class FanManager : MonoBehaviour {
 
+	public float Power = 10;
 	private bool _flag;
 	private GameObject _trap;
+	private Animator _animetor;
 	public string TrapName = "SEKITAN";
 
 	void Start( ) {
 		_flag = false;
 		_trap = GameObject.Find( TrapName ).gameObject;
+		_animetor = GetComponent< Animator >( );
 	}
 	
 	void Update( ) {
 		if ( _flag ) {
-			Destroy ( _trap );
+			Vector3 pos = transform.position - _trap.transform.position ;
+			_trap.GetComponent< Rigidbody >( ).AddForce( pos.x * Power, pos.y * Power, pos.z * Power );
+			_animetor.SetBool( "Start", true );
 		}
 	}
 
