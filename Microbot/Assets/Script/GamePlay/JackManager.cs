c98274c9860b601f87681b49927e7d704d2e6f8f
sunshine <sunshine233333@gmail.com>
2016-09-03 14:22:20 +0900

    ロボットのモーションの改善

--- Microbot/Assets/Script/GamePlay/JackManager.cs
+++ Microbot/Assets/Script/GamePlay/JackManager.cs
@@ -3,44 +3,43 @@ using System.Collections;
 
 public class JackManager : MonoBehaviour {
 
-    public float MaxScaleY = 10;
+    public float MaxScaleY = 0.5f;
 	private float _scale_y;
-	private float _old_scale_y;
-	private float _gauge;
 	private GameObject _set;
+	private bool _flag;
 	public string TrapName = "JackSet";
 
 	void Start( ) {
-        _old_scale_y = transform.localScale.y;
-		_gauge = 100;
+		_flag = false;
+		_scale_y = 0;
 		_set = GameObject.Find( TrapName ).gameObject;
 	}
 
 	void Update( ) {
-		_scale_y = _gauge * 1.0f / 50.0f;
-
+		if ( _flag && _scale_y <= MaxScaleY ) {
+			_scale_y += 0.01f;
+			playJack ( );
+		}
 	}
 
-	public void playJack( ) {
+	private void playJack( ) {
         Vector3 col = GetComponent<BoxCollider>( ).size;
         Vector3 center = GetComponent<BoxCollider>().center;
-        center.y += ( Mathf.Abs( _old_scale_y - _scale_y) ) / 2;
-        col.y += ( Mathf.Abs( _old_scale_y - _scale_y ) ) / 2;
+        center.y += _scale_y / 2;
+        col.y += _scale_y / 2;
         GetComponent<BoxCollider>( ).size = col;
         GetComponent<BoxCollider>().center = center;
 
      
-        if ( _old_scale_y != _scale_y ) {
-		    Vector3 s_pos = _set.transform.position;
-            s_pos.y += (Mathf.Abs( _old_scale_y - _scale_y) ) / 2;
-		    _set.transform.position = s_pos;
-        }
-        _old_scale_y = _scale_y;
+		Vector3 s_pos = _set.transform.position;
+        s_pos.y += (Mathf.Abs( _scale_y ) ) / 2;
+		_set.transform.position = s_pos;
 	}
 
-	public void giveGauge( float gauge ) {
-        if (_scale_y < MaxScaleY) {
-			_gauge += gauge;
-		}
+	public void play( ) {
+		_flag = true;
+	}
+	public bool getPlay () {
+		return _flag;
 	}
 }
