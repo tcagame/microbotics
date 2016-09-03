5d84133ab3633b10522c7a2fd5a48e2d75666598
金パラ正和 <ebipilaf000@gmail.com>
2016-09-03 11:54:40 +0900

    プロペラ

--- Microbot/Assets/Script/GamePlay/Propellers.cs
+++ Microbot/Assets/Script/GamePlay/Propellers.cs
@@ -1,21 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Propeller : MonoBehaviour {
+public class Propellers : MonoBehaviour {
 
 	private GameObject _propeller;
 	private GameObject _fan;
+	private GameObject _fan_range;
 	public string PropellerName;
 	public string FanName;
+	public string RangeName;
 	private GameObject _player;
 
 	bool _tracking_flag;
 	bool _flying_flag;
+
     private const float CONNECT_RANGE = 2.0f;
+
 
 	void Start ( ) {
 		_propeller = GameObject.Find( PropellerName ).gameObject;
 		_fan = GameObject.Find( FanName ).gameObject;
+		_fan_range = GameObject.Find (RangeName).gameObject;
 		_player = GameObject.Find ( "Player" ).gameObject;
 		_tracking_flag = false;
 		_flying_flag = false;
@@ -25,6 +30,9 @@ public class Propeller : MonoBehaviour {
 		judgeTracking( );
 		if ( _tracking_flag ) {
 			setPosition( );
+		}
+		if (_flying_flag) {
+			setFlying();
 		}
 	}
 
@@ -47,5 +55,28 @@ public class Propeller : MonoBehaviour {
 			player_pos.y += 1.5f;
 			_propeller.transform.position = player_pos;
 		}
+	}
+
+	private void setFlying( ) {
+		Vector3 player_pos = _player.transform.position;
+		player_pos.y += 1.5f * Time.deltaTime;
+		if (isFindPos()) {
+			player_pos.z += 1.5f * Time.deltaTime;
+		}
+		_player.transform.position = player_pos;
+	}
+
+	private bool isFindPos( ) {
+		Vector3 fan_range_pos = _fan_range.transform.position;
+		Vector3 fan_range_scale = _fan_range.transform.localScale;
+		if ( ( ( fan_range_pos.x - fan_range_scale.x / 2 ) < _fan.transform.position.x ) &&
+			 ( ( fan_range_pos.x + fan_range_scale.x / 2 ) > _fan.transform.position.x ) &&
+			 ( ( fan_range_pos.y - fan_range_scale.y / 2 ) < _fan.transform.position.y ) &&
+			 ( ( fan_range_pos.y + fan_range_scale.y / 2 ) > _fan.transform.position.y ) &&
+			 ( ( fan_range_pos.z - fan_range_scale.z / 2 ) < _fan.transform.position.z ) &&
+			 ( ( fan_range_pos.z + fan_range_scale.z / 2 ) > _fan.transform.position.z ) ) {
+			return true;
+		}
+		return false;
 	}
 }
