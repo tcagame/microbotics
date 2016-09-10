197d81ff2cd57be4e0d5bf28f5244e9d4ed975ff
sunshine <sunshine233333@gmail.com>
2016-09-10 14:04:13 +0900

    カメラ＆タイトル画面の変更

--- Microbot/Assets/Script/GamePlay/CameraController.cs
+++ Microbot/Assets/Script/GamePlay/CameraController.cs
@@ -2,13 +2,18 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-	private float[] _before_angle = new float[ 2 ];
-
+	private Operation _operation;
 	private GameObject _player;
+	private Vector3 _start_pos1;
+	private Vector3 _start_pos2;
+
+	private Vector3 _transfrom;
 	private Vector3 _camera_vec;
+	private Vector3 _camera_lock;
 	private float _angle = 0;
 
 	void Awake( ) {
+		_operation = GameObject.Find( "Operation" ).GetComponent< Operation >( );
 		if ( _player == null ) {
 			GameObject nullplayer = GameObject.Find ("Player");
 			if ( nullplayer == null ) {
@@ -21,16 +26,55 @@ public class CameraController : MonoBehaviour {
 	// Use this for initialization
 	void Start( ){
 		_camera_vec = transform.position - _player.transform.position;
+		_transfrom = _player.transform.position + _camera_vec;
+		_camera_lock = new Vector3 ( );
 	}
 	
 	// Update is called once per frame
 	void Update( ) {
+		_transfrom = _player.transform.position + _camera_vec;
 		tracking( );
 		turn( );
+
 	}
 
 	void turn( ) {
-		if ( Input.touchCount >= 2 ) {
+		_angle = 0;
+		Vector3 move_pos1 = new Vector3 ();
+		Vector3 move_pos2 = new Vector3 ();
+		if ( _operation.getInputCount( ) == 1 ) {
+			Vector3 pos = _operation.getInputPosition( 0 );
+			if ( pos.x > 50 && pos.x < 400 ) {
+				return;
+			}
+			if ( _operation.getTouchPhase( ) == TouchPhase.Began ) {
+				_start_pos1 = pos;
+			}
+			if ( _operation.getTouchPhase( ) == TouchPhase.Moved ) {
+				move_pos1 = pos - _start_pos1;
+			}
+			if ( move_pos1.y == 0 ) {
+				return;
+			}
+			_angle = move_pos1.y / 5;
+			transform.RotateAround (_player.transform.position , Vector3.up, _angle);
+		}
+		if ( _operation.getInputCount( ) == 2 ) {
+			Vector3 pos1 = _operation.getInputPosition( 0 );
+			Vector3 pos2 = _operation.getInputPosition( 1 );
+			if ( pos1.x > 50 && pos1.x < 400 ) {
+				return;
+			}
+			if ( _operation.getTouchPhase( 0 ) == TouchPhase.Began ) {
+				_start_pos1 = pos1;
+			}
+			if ( _operation.getTouchPhase( 0 ) == TouchPhase.Moved ) {
+				move_pos1 = pos1 - _start_pos1;
+			}
+			_camera_lock += move_pos1;
+		}
+
+		/*if ( Input.touchCount >= 2 ) {
 			Vector2 center_pos = ( Input.GetTouch( 0 ).position + Input.GetTouch( 1 ).position ) / 2;
 			Vector2 vec1 = Input.GetTouch( 0 ).position - center_pos;
 			Vector2 vec2 = Input.GetTouch( 1 ).position - center_pos;
@@ -58,11 +102,12 @@ public class CameraController : MonoBehaviour {
 				_before_angle[ 0 ] = angle_1;
 				_before_angle[ 1 ] = angle_2;
 			}
-		}
+		}*/
 	}
 
 	void tracking( ) {
-		transform.position = new Vector3 ( _player.transform.position.x, _player.transform.position.y, _player.transform.position.z ) + _camera_vec;
-		transform.LookAt(_player.transform.position + ( _player.transform.up * ( _player.transform.localScale.y / 2 + _angle ) ) );
+		transform.position = _transfrom;
+
+		transform.LookAt(_player.transform.position + ( _player.transform.up * ( _player.transform.localScale.y / 2 + _angle  ) ) +_camera_lock );
 	}
 }
