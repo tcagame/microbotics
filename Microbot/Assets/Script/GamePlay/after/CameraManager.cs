23c975f1fccb9b976507db1714f3f53bfd9ad00f
Masato <masato12021996@gmail.com>
2016-10-22 15:58:56 +0900

    カメラ更新

--- Microbot/Assets/Script/GamePlay/after/CameraManager.cs
+++ Microbot/Assets/Script/GamePlay/after/CameraManager.cs
@@ -3,13 +3,55 @@ using System.Collections;
 
 public class CameraManager : MonoBehaviour {
 
+	private enum CAMERA_MODE {
+		PLAY,
+		EVENT,
+		STAGE_VIEW
+	};
+	private PlayCamera _play_camera;
+	private EventCamera _event_camera;
+	private StageViewCamera _stage_view_camera;
+	private CAMERA_MODE _camera_mode; 
+
+	void Awake( ) {
+		_play_camera = new PlayCamera( );
+		_event_camera = new EventCamera( );
+		_stage_view_camera = new StageViewCamera( );
+		_camera_mode = CAMERA_MODE.PLAY;
+	}
+
 	// Use this for initialization
 	void Start () {
-	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		switch ( _camera_mode ) {
+		case CAMERA_MODE.PLAY:
+			_play_camera.update( );
+			break;
+		case CAMERA_MODE.EVENT:
+			_event_camera.update( );
+			if ( _event_camera.finish( ) ) {
+				_camera_mode = CAMERA_MODE.PLAY;
+			}
+			break;
+		case CAMERA_MODE.STAGE_VIEW:
+			_stage_view_camera.update( );
+			if ( _stage_view_camera.finish( ) ) {
+				_camera_mode = CAMERA_MODE.PLAY;
+			}
+			break;
+		}
+	}
+
+	public void callEventCamera( Vector3 target, Vector3 pos ) {
+		_event_camera.callEnventCamera( target, pos );
+		_camera_mode = CAMERA_MODE.EVENT;
+	}
+
+	public void callStageViewCamera( ) {
+		_stage_view_camera.initialize( );
+		_camera_mode = CAMERA_MODE.STAGE_VIEW;
 	}
 }
--- Microbot/Assets/Script/GamePlay/after/EventCamera.cs
+++ Microbot/Assets/Script/GamePlay/after/EventCamera.cs
@@ -1,16 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EventCamera : MonoBehaviour {
-	
+public class EventCamera : Monobehavior {
+	private double WAIT_TIME = 5 * 60;
+	private double _time;
+	private Camera _mine;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Awake( ) {
+		_time = 0;
+		_mine = gameObject.GetComponent< Camera >( );
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update( ) {
+		_time += Time.deltaTime;
+	}
+
+	public void CallEventCamera( Vector3 pos, Vector3 target ) {
+		if ( _time != 0 ) {
+			return;
+		}
+		_mine.transform.position = pos;
+		_mine.transform.LookAt( target );
+	}
+
+	public bool finish( ) {
+		bool result = false;
+		if ( _time > WAIT_TIME ) {
+			result = true;
+		}
+		return result;
 	}
 }
