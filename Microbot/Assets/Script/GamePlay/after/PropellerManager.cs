cea2c48b82e812be140228f7a6108b5584ee4ef7
金原正和 <ebipilaf000@gmail.com>
2016-11-05 14:41:50 +0900

    プラペラエラー修正

--- Microbot/Assets/Script/GamePlay/after/PropellerManager.cs
+++ Microbot/Assets/Script/GamePlay/after/PropellerManager.cs
@@ -2,22 +2,53 @@
 using System.Collections;
 
 public class PropellerManager : MonoBehaviour {
+	private enum STATE {
+		STATE_UP,
+		STATE_DOWN
+	}
+
+	private enum PLACE {
+		PLACE_3
+	}
+
 	private GameObject _propeller;
 	public string PropellerName;
 	private GameObject _player;
+	public string PlayerName;
 
 	public const float FLY_SPEED = 0.05f;
 
-	private Vector3 PROPELLER_START_POS = new Vector3 ( -15.0f, 1.8f, 0.0f);
+	public Vector3 PROPELLER_START_POS = new Vector3( -15.0f, 1.8f, 0.0f );
 	private float MAX_HEIGHT = 9.0f;
-	
+
+	private bool _flag;
+
+
+
+	void Awake( ) {
+		_propeller = GameObject.Find (PropellerName).gameObject;
+		_propeller.gameObject.transform.position = PROPELLER_START_POS;
+	}
 	// Use this for initialization
-	void Start () {
-	
+	void Start( ) {
+		_player = GameObject.Find( PlayerName ).gameObject;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update( ) {
+		
+	}
+
+
+
+
+
+
+	public void action( ) {
+		_flag = true;
+	}
+
+	public bool isActive( ) {
+		return _flag;
 	}
 }
